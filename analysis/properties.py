4d6c27b18d542da4457419f112237cf6ff0ed92e
manosgior <giortan13@gmail.com>
2025-02-11 16:34:23 +0100

    Create properties.py

---
+++ analysis/properties.py
@@ -0,0 +1,16 @@
+from qiskit.circuit import QuantumCircuit
+
+def countGates(circuit: QuantumCircuit, gates: list[str]):
+    pass
+
+def countNonLocalGates(circuit: QuantumCircuit, gates: list[str] = ["cx", "ecr", "cz", "rzz"]):
+    pass
+
+def countRemoteGates(circuit: QuantumCircuit, remote_couplings: list[tuple]):
+    pass
+
+def getDepth(circuit: QuantumCircuit):
+    pass
+
+
+    
